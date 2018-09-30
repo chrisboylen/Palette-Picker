38b382eeb25842b80ab647b450ddb832325cea0d
Chris Boylen <billpolster@gmail.com>
2018-09-30 09:51:24 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public/'));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Palette Picker';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Palette Picker');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
