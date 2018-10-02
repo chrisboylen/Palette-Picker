755641819a57e27f32483adf9790195e087ee9fc
Chris Boylen <billpolster@gmail.com>
2018-10-02 17:45:07 -0600

    Refactor user generate random color palette

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,3 @@
-// document.querySelector('#save-palette-input').addEventListener('focus blur', function (e) {
-//   $(this).parents('.float-container').classList.toggle('is-focused', (e.type === 'focus' || this.value.length > 0))
-//   // document.querySelector('.float-container').classList.toggle('active');
-// }).trigger('blur');
-
-// document.querySelector('#save-palette-input').addEventListener('blur', () => {
-//   document.querySelector('.float-container').classList.remove('active');
-// });
-
-// $(document).ready(getColors);
-
-
 const generateRandomPalette = () => {
   const randomColor = () => "#" + (Math.random().toString(16) + "000000").slice(2, 8).toLocaleUpperCase();
   
@@ -23,4 +11,4 @@ const generateRandomPalette = () => {
   }
 };
 
-$('.generate-btn').on('click', generateRandomPalette)
+$('.generate-btn').on('click', generateRandomPalette);
