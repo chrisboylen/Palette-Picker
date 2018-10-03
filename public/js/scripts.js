144aad2eb81cf441be384f444b885a0894439df2
Chris Boylen <billpolster@gmail.com>
2018-10-02 21:02:42 -0600

    Add ability to lock colors when generating new palettes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,11 +4,20 @@ const generateRandomPalette = () => {
   for (let i = 0; i < 5; i++) {
     const newColor = randomColor();
 
-    if (!$(`.color_${i + 1}`).hasClass('saved')) {
+    if (!$(`.color_${i + 1}`).children('img').hasClass('saved')) {
       $(`.color_${i + 1}`).css('background-color', newColor);
       $(`.color_${i + 1}`).children('h3').text(newColor);
     }
   }
 };
 
+function toggleLockColor() {
+  const unlocked = './images/unlocked.svg';
+  const locked = './images/locked.svg';
+
+  $(this).toggleClass('saved');
+  $(this).attr('src', $(this).hasClass('saved') ? locked : unlocked);
+}
+
 $('.generate-btn').on('click', generateRandomPalette);
+$('.unsaved').on('click', toggleLockColor);
